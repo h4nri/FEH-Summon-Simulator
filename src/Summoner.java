490d0ab9b44a6ba22a398f207d8ccb0e5c4ca30a
Henry Zheng <zheng.henreh@gmail.com>
2018-07-03 18:27:51 -0400

    Changed String[] & ArrayList<String> variables into List<String> variables, and simplied code in setupBanner method.

--- src/Summoner.java
+++ src/Summoner.java
@@ -2,66 +2,85 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Summoner {
 	
-	private int pullsWithoutFocusOrFive, totalPulls, redHeroes, blueHeroes, greenHeroes, colorlessHeroes, focusHeroes, fiveHeroes, fourHeroes, threeHeroes; 
+	private int pullsWithoutFocusOrFive, totalPulls, redHeroes, blueHeroes, greenHeroes, colorlessHeroes;
 	
 	private double initialFocusRate, initialFiveRate, initialFourRate, initialThreeRate, currentFocusRate, currentFiveRate, currentFourRate, currentThreeRate, 
 		focusRateIncrease, fiveRateIncrease, fourRateDecrease, threeRateDecrease;
-	
-	private String[] redFocusArray, redFiveArray, redFourArray, redThreeArray, blueFocusArray, blueFiveArray, blueFourArray, blueThreeArray,
-		greenFocusArray, greenFiveArray, greenFourArray, greenThreeArray, colorlessFocusArray, colorlessFiveArray, colorlessFourArray, colorlessThreeArray;
 
-	private ArrayList<String> focusArrayList, fiveArrayList, fourArrayList, threeArrayList, 
-		summonedFocusArrayList, summonedFiveArrayList, summonedFourArrayList, summonedThreeArrayList;
-	
+	private List<String> redFocusList, redFiveList, redFourList, redThreeList, blueFocusList, blueFiveList, blueFourList, blueThreeList,
+		greenFocusList, greenFiveList, greenFourList, greenThreeList, colorlessFocusList, colorlessFiveList, colorlessFourList, colorlessThreeList,
+		focusList, fiveList, fourList, threeList, summonedFocusList, summonedFiveList, summonedFourList, summonedThreeList;
 		
 	public Summoner() {		
-		focusArrayList = new ArrayList<>();
-		fiveArrayList = new ArrayList<>(); 
-		fourArrayList = new ArrayList<>(); 
-		threeArrayList = new ArrayList<>(); 
-		summonedFocusArrayList = new ArrayList<>();
-		summonedFiveArrayList = new ArrayList<>();
-		summonedFourArrayList = new ArrayList<>();
-		summonedThreeArrayList = new ArrayList<>();
+		redFocusList = new ArrayList<String>();
+		redFiveList = new ArrayList<String>();
+		redFourList = new ArrayList<String>();
+		redThreeList = new ArrayList<String>();
+		blueFocusList = new ArrayList<String>();
+		blueFiveList = new ArrayList<String>();
+		blueFourList = new ArrayList<String>();
+		blueThreeList = new ArrayList<String>();
+		greenFocusList = new ArrayList<String>();
+		greenFiveList = new ArrayList<String>();  
+		greenFourList = new ArrayList<String>();
+		greenThreeList = new ArrayList<String>();
+		colorlessFocusList = new ArrayList<String>();
+		colorlessFiveList = new ArrayList<String>();
+		colorlessFourList = new ArrayList<String>();
+		colorlessThreeList = new ArrayList<String>();
 		
-		setupBanner("Scattered Fangs");
+		focusList = new ArrayList<String>();
+		fiveList = new ArrayList<String>(); 
+		fourList = new ArrayList<String>(); 
+		threeList = new ArrayList<String>(); 
+		
+		summonedFocusList = new ArrayList<String>();
+		summonedFiveList = new ArrayList<String>();
+		summonedFourList = new ArrayList<String>();
+		summonedThreeList = new ArrayList<String>();
 	}
 	
+	// TODO: Describe this method.
 	public void pull(int pulls) {
-		if (pulls < 0 || pulls > 5) {
-			System.out.println("Error: Invalid number of pulls entered. Please enter a value from 0-5.");
+		if (pulls < 1 || pulls > 5) {
+			System.out.println("Error: Invalid number of pulls entered. Please enter a value from 1-5.");
 		} else {
+			int counter = 1;
 			boolean focusOrFiveSummoned = false;
-	
-			for (int i = 0; i <= pulls; i++) {
+			
+			while (counter <= pulls) {
 				double determineRarity = Math.random();
-				System.out.println("generateRarity: " + determineRarity);
+				System.out.println("determineRarity: " + determineRarity);
 				if ((0.0 <= determineRarity) && (determineRarity < currentFocusRate)) {
-					int determineHero = ThreadLocalRandom.current().nextInt(0, focusHeroes);
-					summonedFocusArrayList.add(focusArrayList.get(determineHero));
+					int determineHero = ThreadLocalRandom.current().nextInt(0, focusList.size());
+					summonedFocusList.add(focusList.get(determineHero));
 					focusOrFiveSummoned = true;
 				} else if ((currentFocusRate <= determineRarity) && (determineRarity < (currentFocusRate + currentFiveRate))) {
-					int determineHero = ThreadLocalRandom.current().nextInt(0, fiveHeroes);
-					summonedFiveArrayList.add(fiveArrayList.get(determineHero));
+					int determineHero = ThreadLocalRandom.current().nextInt(0, fiveList.size());
+					summonedFiveList.add(fiveList.get(determineHero));
 					focusOrFiveSummoned = true;
 				} else if (((currentFocusRate + currentFiveRate) <= determineRarity) && (determineRarity < currentFocusRate + currentFiveRate + currentFourRate)) {
-					int determineHero = ThreadLocalRandom.current().nextInt(0, fourHeroes);
-					summonedFourArrayList.add(fourArrayList.get(determineHero));
+					int determineHero = ThreadLocalRandom.current().nextInt(0, fourList.size());
+					summonedFourList.add(fourList.get(determineHero));
 				} else {
-					int determineHero = ThreadLocalRandom.current().nextInt(0, threeHeroes);
-					summonedThreeArrayList.add(threeArrayList.get(determineHero));
+					int determineHero = ThreadLocalRandom.current().nextInt(0, threeList.size());
+					summonedThreeList.add(threeList.get(determineHero));
 				}
+				counter++;
 			}
+
+			System.out.println("Summoned focus heroes: " + summonedFocusList);
+			System.out.println("Summoned five heroes: " + summonedFiveList);
+			System.out.println("Summoned four heroes: " + summonedFourList);
+			System.out.println("Summoned three heroes: " + summonedThreeList);
 			
-			System.out.println("Summoned focus heroes: " + summonedFocusArrayList);
-			System.out.println("Summoned five heroes: " + summonedFiveArrayList);
-			System.out.println("Summoned four heroes: " + summonedFourArrayList);
-			System.out.println("Summoned three heroes: " + summonedThreeArrayList);
-			
+			// Change appearance rates based on whether or not a focus or 5-star hero was summoned.
 			if (focusOrFiveSummoned == true) {
 				pullsWithoutFocusOrFive = 0;
 				currentFocusRate = initialFocusRate;
@@ -71,7 +90,7 @@ public class Summoner {
 			} else {
 				pullsWithoutFocusOrFive = pullsWithoutFocusOrFive + pulls;
 				
-				// Set current appearance rates based on how many pulls it has been since a focus or 5-star hero was pulled. 
+				// Set current appearance rates based on how many pulls it has been since a focus or 5-star hero was summoned. 
 				int rateChanges = Math.floorDiv(pullsWithoutFocusOrFive, 5);
 				currentFocusRate = initialFocusRate + (rateChanges * focusRateIncrease);
 				currentFiveRate = initialFiveRate + (rateChanges * fiveRateIncrease);
@@ -79,112 +98,93 @@ public class Summoner {
 				currentThreeRate = initialThreeRate - (rateChanges * threeRateDecrease);
 			}
 			
-			totalPulls = totalPulls + pulls;
-			
+			totalPulls = totalPulls + pulls;			
 			System.out.println("Total pulls: " + totalPulls);
 		}
 	}
 	
-	// Changes the values of variables to represent the selected banner.
+	// TODO: Describe this method.
 	private void setupBanner(String bannerName) {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(bannerName + "\\Heroes.txt"));
+			String line;
 			
-			// Populates the String[]s with the summonable heroes.
-			redFocusArray = br.readLine().split(",");
-			redFiveArray = br.readLine().split(",");
-			redFourArray = br.readLine().split(",");
-			redThreeArray = br.readLine().split(",");
-			blueFocusArray = br.readLine().split(",");
-			blueFiveArray = br.readLine().split(",");
-			blueFourArray = br.readLine().split(",");
-			blueThreeArray = br.readLine().split(",");
-			greenFocusArray = br.readLine().split(",");
-			greenFiveArray = br.readLine().split(",");
-			greenFourArray = br.readLine().split(",");
-			greenThreeArray = br.readLine().split(",");
-			colorlessFocusArray = br.readLine().split(",");
-			colorlessFiveArray = br.readLine().split(",");
-			colorlessFourArray = br.readLine().split(",");
-			colorlessThreeArray = br.readLine().split(",");
+			// Fills the color-grade lists (ex. redFocusList).
+			if ((line = br.readLine()).equals("None") == false) 
+				redFocusList = Arrays.asList(line.split(","));
+			if ((line = br.readLine()).equals("None") == false)
+				redFiveList = Arrays.asList(line.split(","));
+			if ((line = br.readLine()).equals("None") == false)
+				redFourList = Arrays.asList(line.split(","));
+			if ((line = br.readLine()).equals("None") == false)
+				redThreeList = Arrays.asList(line.split(","));
+			if ((line = br.readLine()).equals("None") == false)
+				blueFocusList = Arrays.asList(line.split(","));
+			if ((line = br.readLine()).equals("None") == false)
+				blueFiveList = Arrays.asList(line.split(","));
+			if ((line = br.readLine()).equals("None") == false)
+				blueFourList = Arrays.asList(line.split(","));
+			if ((line = br.readLine()).equals("None") == false)
+				blueThreeList = Arrays.asList(line.split(","));
+			if ((line = br.readLine()).equals("None") == false)
+				greenFocusList = Arrays.asList(line.split(","));
+			if ((line = br.readLine()).equals("None") == false)
+				greenFiveList = Arrays.asList(line.split(","));
+			if ((line = br.readLine()).equals("None") == false)
+				greenFourList = Arrays.asList(line.split(","));
+			if ((line = br.readLine()).equals("None") == false)
+				greenThreeList = Arrays.asList(line.split(","));
+			if ((line = br.readLine()).equals("None") == false)
+				colorlessFocusList = Arrays.asList(line.split(","));
+			if ((line = br.readLine()).equals("None") == false)
+				colorlessFiveList = Arrays.asList(line.split(","));
+			if ((line = br.readLine()).equals("None") == false)
+				colorlessFourList = Arrays.asList(line.split(","));
+			if ((line = br.readLine()).equals("None") == false)
+				colorlessThreeList = Arrays.asList(line.split(","));	
+		
 			br.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}	
-
-		// Calculates and stores total values.
-		// Ex. determines the total number of focus heroes and stores that value in "focusHeroes".
-		redHeroes = redFocusArray.length + redFiveArray.length + redFourArray.length + redThreeArray.length;
-		blueHeroes = blueFocusArray.length + blueFiveArray.length + blueFourArray.length + blueThreeArray.length;
-		greenHeroes = greenFocusArray.length + greenFiveArray.length + greenFourArray.length + greenThreeArray.length;
-		colorlessHeroes = colorlessFocusArray.length + colorlessFiveArray.length + colorlessFourArray.length + colorlessThreeArray.length;
-		focusHeroes = redFocusArray.length + blueFocusArray.length + greenFocusArray.length + colorlessFocusArray.length;
-		fiveHeroes = redFiveArray.length + blueFiveArray.length + greenFiveArray.length + colorlessFiveArray.length;
-		fourHeroes = redFourArray.length + blueFourArray.length + greenFourArray.length + colorlessFourArray.length;
-		threeHeroes = redThreeArray.length + blueThreeArray.length + greenThreeArray.length + colorlessThreeArray.length;
 		
-		// Adds all focus heroes into "focusArrayList".
-		for (int i = 0; i < focusHeroes; i++) {
-			if (i < redFocusArray.length) {
-				focusArrayList.add(redFocusArray[i]);
-			} else if ((redFocusArray.length <= i) && (i < (redFocusArray.length + blueFocusArray.length))) {
-				focusArrayList.add(blueFocusArray[i - redFocusArray.length]);
-			} else if (((redFocusArray.length + blueFocusArray.length) <= i) && (i < (redFocusArray.length + blueFocusArray.length + greenFocusArray.length))) {
-				focusArrayList.add(greenFocusArray[i - redFocusArray.length - blueFocusArray.length]);
-			} else {
-				focusArrayList.add(colorlessFocusArray[i - redFocusArray.length - blueFocusArray.length - greenFocusArray.length]);
-			}
-		}
+		// Fills the grade lists (ex. focusList).
+		focusList.addAll(redFocusList);
+		focusList.addAll(blueFocusList);
+		focusList.addAll(greenFocusList);
+		focusList.addAll(colorlessFocusList);
 		
-		System.out.println("Focus heroes: " + focusArrayList);
+		System.out.println("Focus heroes: " + focusList);
 		
-		// Adds all five heroes into "fiveArrayList".
-		for (int i = 0; i < fiveHeroes; i++) {
-			if (i < redFiveArray.length) {
-				fiveArrayList.add(redFiveArray[i]);
-			} else if ((redFiveArray.length <= i) && (i < (redFiveArray.length + blueFiveArray.length))) {
-				fiveArrayList.add(blueFiveArray[i - redFiveArray.length]);
-			} else if (((redFiveArray.length + blueFiveArray.length) <= i) && (i < (redFiveArray.length + blueFiveArray.length + greenFiveArray.length))) {
-				fiveArrayList.add(greenFiveArray[i - redFiveArray.length - blueFiveArray.length]);
-			} else {
-				fiveArrayList.add(colorlessFiveArray[i - redFiveArray.length - blueFiveArray.length - greenFiveArray.length]);
-			}
-		}
+		fiveList.addAll(redFiveList);
+		fiveList.addAll(blueFiveList);
+		fiveList.addAll(greenFiveList);
+		fiveList.addAll(colorlessFiveList);
 		
-		System.out.println("5-star heroes: " + fiveArrayList);
+		System.out.println("5-star heroes: " + fiveList);
 		
-		// Adds all four heroes into "fourArrayList".
-		for (int i = 0; i < fourHeroes; i++) {
-			if (i < redFourArray.length) {
-				fourArrayList.add(redFourArray[i]);
-			} else if ((redFourArray.length <= i) && (i < (redFourArray.length + blueFourArray.length))) {
-				fourArrayList.add(blueFourArray[i - redFourArray.length]);
-			} else if (((redFourArray.length + blueFourArray.length) <= i) && (i < (redFourArray.length + blueFourArray.length + greenFourArray.length))) {
-				fourArrayList.add(greenFourArray[i - redFourArray.length - blueFourArray.length]);
-			} else {
-				fourArrayList.add(colorlessFourArray[i - redFourArray.length - blueFourArray.length - greenFourArray.length]);
-			}
-		}
+		fourList.addAll(redFourList);
+		fourList.addAll(blueFourList);
+		fourList.addAll(greenFourList);
+		fourList.addAll(colorlessFourList);
 		
-		System.out.println("4-star heroes: " + fourArrayList);
+		System.out.println("4-star heroes: " + fourList);
 		
-		// Adds all three heroes into "threeArrayList".
-		for (int i = 0; i < threeHeroes; i++) {
-			if (i < redThreeArray.length) {
-				threeArrayList.add(redThreeArray[i]);
-			} else if ((redThreeArray.length <= i) && (i < (redThreeArray.length + blueThreeArray.length))) {
-				threeArrayList.add(blueThreeArray[i - redThreeArray.length]);
-			} else if (((redThreeArray.length + blueThreeArray.length) <= i) && (i < (redThreeArray.length + blueThreeArray.length + greenThreeArray.length))) {
-				threeArrayList.add(greenThreeArray[i - redThreeArray.length - blueThreeArray.length]);
-			} else {
-				threeArrayList.add(colorlessThreeArray[i - redThreeArray.length - blueThreeArray.length - greenThreeArray.length]);
-			}
-		}
+		threeList.addAll(redThreeList);
+		threeList.addAll(blueThreeList);
+		threeList.addAll(greenThreeList);
+		threeList.addAll(colorlessThreeList);
 		
-		System.out.println("3-star heroes: " + threeArrayList);
+		System.out.println("3-star heroes: " + threeList);
+		
+		// Calculates and stores the total number of heroes for each color. Do I really need these variables?
+		redHeroes = redFocusList.size() + redFiveList.size() + redFourList.size() + redThreeList.size();
+		blueHeroes = blueFocusList.size() + blueFiveList.size() + blueFourList.size() + blueThreeList.size();
+		greenHeroes = greenFocusList.size() + greenFiveList.size() + greenFourList.size() + greenThreeList.size();
+		colorlessHeroes = colorlessFocusList.size() + colorlessFiveList.size() + colorlessFourList.size() + colorlessThreeList.size();
 		
 		// Legendary banners do not contain 5-star heroes (aside from the focus ones), and therefore have different appearance rates.
-		if (fiveHeroes != 0) {
+		if (fiveList.size() != 0) {
 			initialFocusRate = 0.03;
 			initialFiveRate = 0.03;
 			initialFourRate = 0.58;
@@ -200,21 +200,20 @@ public class Summoner {
 			fourRateDecrease = 0.0019;
 			threeRateDecrease = 0.0031;
 		} else {
-			// TODO: Fix these rates.
 			initialFocusRate = 0.08;
 			initialFiveRate = 0.0;
 			initialFourRate = 0.58;
-			initialThreeRate = 0.36;
+			initialThreeRate = 0.34;
 			
 			currentFocusRate = 0.08;
 			currentFiveRate = 0.0;
 			currentFourRate = 0.58;
-			currentThreeRate = 0.36;
+			currentThreeRate = 0.34;
 			
 			focusRateIncrease = 0.005;
 			fiveRateIncrease = 0.0;
-			fourRateDecrease = 0.0019;
-			threeRateDecrease = 0.0031;
+			fourRateDecrease = 0.0032;
+			threeRateDecrease = 0.0018;
 		}
 		
 		pullsWithoutFocusOrFive = 0;
@@ -223,6 +222,7 @@ public class Summoner {
 	
 	public static void main(String[] args) {
 		Summoner summoner = new Summoner();
+		summoner.setupBanner("Scattered Fangs");
 		summoner.pull(5);
 	}
 }
